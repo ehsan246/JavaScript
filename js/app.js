d14ddff90e8fa248963a2091f33b2bdfff1172b2
= <=>
2020-09-21 20:55:33 +0600

    one lesson

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+
+
+
+//////////////////////////////////////////
+//////          SECTION #1          //////
+//////////////////////////////////////////
+
+    // // Log to console
+    // console.log('hello world'); //string
+    // console.log(123); //integer
+    // console.log(true);// bullian
+    // var greeting = 'Hello';
+    // console.log(greeting);//vaiable
+    // console.log([1,2,3,4]);// array
+    // console.log({a:1,b:2});
+    // console.table({a:1,b:2});
+
+    // console.error('this in an error');
+
+    // console.clear();
+    // console.warn('this in an warning');
+
+
+    // console.time('hellow');
+    //     console.log('hello world');
+    //     console.log('hello world');
+    //     console.log('hello world');
+    //     console.log('hello world');
+    // console.timeEnd('hellow');
+
+
+
+/*
+multi
+line 
+comments
+*/
