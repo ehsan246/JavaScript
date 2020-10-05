fa9e733670e90acd5fa4b25c9da681f2abe8ec47
= <=>
2020-10-05 22:14:13 +0600

    es6

--- 05 Object Oriented JavaScript - ES5 ESE5/048 ES6/app.js
+++ 05 Object Oriented JavaScript - ES5 ESE5/048 ES6/app.js
@@ -1,15 +1,54 @@
 class Person {
-    constructor(firstName, lastName){
+    constructor(firstName, lastName, dob){
         this.firstName = firstName;
         this.lastName = lastName;
+        this.birthday = new Date(dob);
     }
 
     greeting(){
         return `Hello there ${this.firstName} ${this.lastName}`
     }
+
+    calculateAge(){
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+
+    getsMarried(newLastName){
+        this.lastName = newLastName;
+    }
+
+
+    static addNumbers(x, y){
+        return x + y;
+    }
 }
 
-const mary = new Person('Mary', 'Williams');
+// SUB CLASS
+class Customer extends Person {
+    constructor(firstName, lastName, phone, membership){
+        super(firstName, lastName);
+
+        this.phone = phone;
+        this.membership = membership;
+    }
+
+}
 
 
-console.log(mary);
+const john = new Customer('John', 'Doe', '555-555-555', 'Standard');
+
+
+console.log(john.greeting());
+
+
+
+
+const mary = new Person('Mary', 'Williams',);
+
+
+console.log(mary.calculateAge());
+
+
+console.log(Person.addNumbers(1,3));
